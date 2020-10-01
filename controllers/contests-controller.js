8036f603f96469f6c79c90496f2297932b2d905f
minecodebot <62449777+minecodebot@users.noreply.github.com>
2020-10-01 14:18:06 +0100

    Update contests-controller.js

--- controllers/contests-controller.js
+++ controllers/contests-controller.js
@@ -265,7 +265,7 @@ exports.getUsersGitQuery = async (req, res) => {
         responseArr.forEach((element) => {
           responseArrFinal = responseArrFinal.concat(element.data.filter((repo) => {return repo.license != null}));
         });
-        res.send(responseArrFinal[0]);
+        res.send(responseArrFinal);
       });
     })
     .catch((err) => {
