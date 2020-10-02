b5c92fb703b936023eaa0caf4c11c197e7507b35
Cláudio Henriques <c.henriques@ua.pt>
2020-10-02 14:44:44 +0100

    Update contests-route.js

--- routes/contests-route.js
+++ routes/contests-route.js
@@ -9,22 +9,7 @@ const router = express.Router()
 router.use(function timeLog(req, res, next) {
   next()
 })
-router.get('/user', contestsRoutes.getAllUser)
-router.get('/user/:userId', contestsRoutes.getUser)
-router.post('/user/:userId/:firstName/:lastName/:imageUrl', contestsRoutes.user)
-router.get('/global', contestsRoutes.getScore)
-router.get('/global/:contestId', contestsRoutes.getScoreByContest)
-router.get('/global/:contestId/:challengeId', contestsRoutes.getScoreByContestChallenge)
-router.get('/:userId', contestsRoutes.getScoreByUser)
-router.get('/list/:userId', contestsRoutes.getListByUser)
-router.get('/:userId/:contestId/', contestsRoutes.getScoreByContestUser)
-router.get('/:userId/:contestId/:challengeId/', contestsRoutes.getScoreByContestChallengeUser)
-router.get("/contents/:contest/:challenge/:userId", contestsRoutes.getContentsGithub)
-router.post('/:userId/:contestId/:challengeId/:score', contestsRoutes.scoreUserChallenge)
-router.get('/network/git/query/:query', contestsRoutes.getUsersGitQuery)
-router.get('/network/git/repos/user/:user', contestsRoutes.getReposUserGit)
-router.get('/network/git/user/:user', contestsRoutes.getUserGit)
-router.get('/network/git/readme/:user/:project', contestsRoutes.getReadmeGit)
+router.get('/github/repos/location/:location', contestsRoutes.getGitReposByLocation)
 
 // Export router
 module.exports = router
