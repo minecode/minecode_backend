1635450c94a516da4ef576c9b6c0c0a8df23b1c7
minecodebot <62449777+minecodebot@users.noreply.github.com>
2021-02-28 19:24:32 +0000

    Create github-controller.js

---
+++ controllers/github-controller.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+require("dotenv").config();
+
+function setCache(response) {
+    response.set("Cache-Control", "public, max-age=300, s-maxage=600");
+}
+
+exports.getGitUser = async (req, res) => {
+    setCache(res);
+    await axios({
+            method: "get",
+            url: `https://api.github.com/users/${req.params.login}`,
+            headers: {
+                Authorization: `token ${process.env.TOKEN}`,
+                "Content-Type": "application/json",
+                Accept: "application/vnd.github.mercy-preview+json", // MUST ADD TO INCLUDE TOPICS
+            },
+        })
+        .then((userData) => {
+            res.send(userData.data)
+        })
+        .catch((err) => {
+            res.json({
+                message: `There was an error: ${err}`,
+            });
+        });
+};
