8c38b6f2daeaaa858bcb762e1e972e164b1de08c
minecodebot <62449777+minecodebot@users.noreply.github.com>
2021-02-28 20:22:00 +0000

    Update github-controller.js

--- controllers/github-controller.js
+++ controllers/github-controller.js
@@ -25,3 +25,24 @@ exports.getGitUser = async (req, res) => {
             });
         });
 };
+
+exports.getGitRepo = async (req, res) => {
+    setCache(res);
+    await axios({
+            method: "get",
+            url: `https://api.github.com/repos/${req.params.repo}`,
+            headers: {
+                Authorization: `token ${process.env.TOKEN}`,
+                "Content-Type": "application/json",
+                Accept: "application/vnd.github.mercy-preview+json", // MUST ADD TO INCLUDE TOPICS
+            },
+        })
+        .then((userData) => {
+            res.send(userData.data)
+        })
+        .catch((err) => {
+            res.json({
+                message: `There was an error: ${err}`,
+            });
+        });
+};
