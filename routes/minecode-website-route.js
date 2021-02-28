b3fc54786cc793878ad85a640f2c3524c429a22a
minecodebot <62449777+minecodebot@users.noreply.github.com>
2021-02-28 18:57:03 +0000

    Update minecode-website-route.js

--- routes/minecode-website-route.js
+++ routes/minecode-website-route.js
@@ -10,9 +10,9 @@ router.use(function timeLog(req, res, next) {
   next()
 })
 router.get('/github/members', contestsRoutes.getGitMembersOfMinecodeWebsite)
+router.get('/github/issues/closed', contestsRoutes.getGitIssuesClosedOfMinecodeWebsite)
 router.get('/github/issues/:id', contestsRoutes.getGitIssueIdOfMinecodeWebsite)
 router.get('/github/issues/:id/comments', contestsRoutes.getGitIssueIdCommentsOfMinecodeWebsite)
-router.get('/github/issues/closed', contestsRoutes.getGitIssuesClosedOfMinecodeWebsite)
 router.get('/github/labels', contestsRoutes.getGitLabelsOfMinecodeWebsite)
 
 // Export router
