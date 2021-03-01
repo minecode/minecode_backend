56aa7f3f3bf645256f350aa599efa7f1b3d1ad6e
Cláudio Henriques <c.henriques@ua.pt>
2021-03-01 15:22:28 +0000

    Update contests-route.js

--- routes/contests-route.js
+++ routes/contests-route.js
@@ -9,8 +9,8 @@ const router = express.Router()
 router.use(function timeLog(req, res, next) {
   next()
 })
-router.get('/github/repos/location/:location', contestsControllers.getGitReposByLocation)
-router.get('/github/repos/:user/:repo/contributors', contestsControllers.getContributorsByRepo)
+router.get('/repos/location/:location', contestsControllers.getGitReposByLocation)
+router.get('/repos/:user/:repo/contributors', contestsControllers.getContributorsByRepo)
 
 // Export router
 module.exports = router
